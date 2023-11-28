33ed0eb92c6bd0aa0d5672b0a94a8b2c74257d2f
ehsanvs65 <vahidiehsans6565@gmail.com>
2023-11-29 01:08:30 +0330

    'newproject'

---
+++ firstapp/Models/Entities/Users.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace firstapp.Models.Entities
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        [EmailAddress(ErrorMessage = "ایمیل را به درستی وارد کنید")]
+        [Required(ErrorMessage = "لطفا ایمیل را وارد کنید")]
+
+        public string Email { get; set; }
+    }
+}
